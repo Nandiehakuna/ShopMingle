60b73bccd2d94cbe11dffd713b499c3afe5be0f0
Bakarijack <bakarikilu254@gmail.com>
2023-10-15 10:29:10 +0300

    progress bar ui complete

--- src/components/form/BusinessRegisterForm.js
+++ src/components/form/BusinessRegisterForm.js
@@ -16,7 +16,10 @@ const Container =styled.div`
 `
 
 const ProgressContainer = styled.div`
-
+  margin-bottom: 1.5em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const Wrapper = styled.div`
@@ -34,12 +37,38 @@ const InnerWrapper = styled.div`
   grid-auto-rows: 1fr;
   grid-auto-flow: dense;
 `
+const Span = styled.span`
+  padding: 0.5em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 0.5em;
+  width: 0.5em;
+  border-radius: 50%;
+  background-color: var(--success-bg-color);
+  color: var(--success-color);
+  text-align: center;
+`
+
+const Hr = styled.hr`
+  border: 1px solid var(--success-bg-color);
+  width: 50%;
+  height: 0;
+  margin: 0;
+`
+
 
 const BusinessRegisterForm = () => {
   return (
     <Main>
         <Wrapper>
-            <ProgressContainer>progress</ProgressContainer>
+            <ProgressContainer>
+                <Span>1</Span>
+                <Hr/>
+                <Span>2</Span>
+                <Hr/>
+                <Span>3</Span>
+            </ProgressContainer>
             <Form>
                 <Container>
                     <Container>1. Business Details</Container>
