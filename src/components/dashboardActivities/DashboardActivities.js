e797cba666c1194eebf8eb3721e596da39ab3552
Bakarijack <bakarikilu254@gmail.com>
2023-10-16 10:01:53 +0300

    adding dashboard sections to dashbord layout

---
+++ src/components/dashboardActivities/DashboardActivities.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardActivities = () => {
+  return (
+    <div>DashboardActivities</div>
+  )
+}
+
+export default DashboardActivities
--- src/pages/dashboardPage/DashBoardPage.js
+++ src/pages/dashboardPage/DashBoardPage.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import DashboardNavigation from '../../components/dashboardNavigation/DashboardNavigation'
+import DashboardHeader from '../../components/dashboardHeader/DashboardHeader'
+import DashboardActivities from '../../components/dashboardActivities/DashboardActivities'
 
 const DashBoardPage = () => {
   return (
     <section>
+        <DashboardNavigation/>
+        <DashboardHeader/>
+        <DashboardActivities/>
         <Outlet/>
     </section>
   )
